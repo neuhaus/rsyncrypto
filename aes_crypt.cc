44ebefe3c09194802ec96842db44d613455d9654
Shachar Shemesh <Shachar@users.noreply.github.com>
2005-01-28 06:58:29 +0000

    Snapshot

---
+++ aes_crypt.cc
@@ -0,0 +1,25 @@
+#include <stdint.h>
+
+#include <openssl/rand.h>
+
+#include "rsyncrypto.h"
+#include "aes_crypt.h"
+
+aes_key::aes_key( uint16_t key_size,  uint32_t sum_span, uint32_t sum_mod, uint32_t sum_min_dist,
+        const unsigned char *buffer ) : key(key_size, CYPHER_AES, sum_span, sum_mod, sum_min_dist),
+                                        secret_key(new unsigned char[key_size])
+{
+    const aes_export *import=reinterpret_cast<const aes_export *>(buffer);
+
+    memcpy( aes_header.iv, import->iv, sizeof(aes_header.iv) );
+    memcpy( secret_key.get(), import->key, key_size );
+}
+
+aes_key::aes_key( size_t keybits, uint32_t sum_span, uint32_t sum_mod, uint32_t sum_min_dist ) :
+    key((keybits+7)/8, CYPHER_AES, sum_span, sum_mod, sum_min_dist ),
+    secret_key(new unsigned char[header.key_size])
+{
+    if( !RAND_bytes(aes_header.iv, sizeof( aes_header.iv )) ||
+                !RAND_bytes(secret_key.get(), header.key_size) )
+            throw rscerror("No random entropy for key and IV");
+}
--- aes_crypt.h
+++ aes_crypt.h
@@ -2,12 +2,22 @@
 #define AES_CRYPT_H
 
 #include "crypt_key.h"
+#include "autoarray.h"
+#include <openssl/aes.h>
 
 class aes_key : public key {
+    struct aes_export {
+        unsigned char iv[AES_BLOCK_SIZE];
+        unsigned char key[0];
+    } aes_header;
+    auto_array<unsigned char> secret_key;
 public:
     aes_key( uint16_t key_size,  uint32_t sum_span, uint32_t sum_mod, uint32_t sum_min_dist,
-            const unsigned char *buffer ) : key(key_size, CYPHER_AES, sum_span, sum_mod, sum_min_dist)
+            const unsigned char *buffer );
+    aes_key( size_t keybits, uint32_t sum_span, uint32_t sum_mod, uint32_t sum_min_dist );
+    virtual size_t block_size() const
     {
+        return AES_BLOCK_SIZE;
     }
 };
 
