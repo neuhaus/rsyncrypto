c2118d7888902674fd6c5a9e63644ac301711f64
Shachar Shemesh <Shachar@users.noreply.github.com>
2005-02-01 14:57:19 +0000

    Change the RSA padding algorithm Add padding algorithm version identifier (plaintext) at the start of the file

--- crypto.h
+++ crypto.h
@@ -31,6 +31,7 @@ enum CYPHER_TYPE { CYPHER_AES };
 //struct key_header *gen_header(int key_length, enum CYPHER_TYPE cypher);
 key *read_header( int headfd );
 void write_header( const char *filename, const key *head );
+size_t header_size( const RSA *rsa );
 void encrypt_header( const struct key_header *header, RSA *rsa, unsigned char *to );
 RSA *extract_public_key( const char *pem_filename );
 RSA *extract_private_key( const char *key_filename );
