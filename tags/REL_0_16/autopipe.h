897fb9ed787baf86db2c11d40f8f3041ba9e68f2
nobody <nobody@eaec4259-e421-0410-a8d7-a676883547c7>
2005-11-04 23:39:08 +0000

    This commit was manufactured by cvs2svn to create tag 'REL_0_16'.

---
+++ tags/REL_0_16/autopipe.h
@@ -0,0 +1,62 @@
+// This is the Win32 version of this struct
+
+#if defined(_WIN32)
+#include "win32/autopipe.h"
+#else
+#ifndef _AUTOPIPE_H
+#define _AUTOPIPE_H
+
+class autopipe {
+    autofd input, output;
+public:
+    explicit autopipe(size_t pipe_size=4096) : input(-1), output(-1)
+    {
+        int fd[2];
+
+        if( pipe(fd)==0 ) {
+            input=autofd(fd[0]);
+            output=autofd(fd[1]);
+        } else {
+#if defined(EXCEPT_CLASS)
+            throw EXCEPT_CLASS("Couldn't create pipe", errno );
+#endif
+        }
+    }
+    // Default copy constructor and operator= do exactly what we want.
+    //autopipe( const autopipe &that )
+    //autopipe &operator=( const autopipe &that )
+    // As does default dtor
+    //~autopipe()
+    autofd &get_read()
+    {
+        return input;
+    }
+    const autofd &get_read() const
+    {
+        return input;
+    }
+    autofd &get_write()
+    {
+        return output;
+    }
+    const autofd &get_write() const
+    {
+        return output;
+    }
+    void clear_read()
+    {
+        input.clear();
+    }
+    void clear_write()
+    {
+        output.clear();
+    }
+    void clear()
+    {
+        clear_read();
+        clear_write();
+    }
+};
+
+#endif // _AUTOPIPE_H
+#endif // _WIN32
