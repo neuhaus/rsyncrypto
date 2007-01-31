618a483d24ef95ec33187d3af3218eec463ff359
thesun <thesun@eaec4259-e421-0410-a8d7-a676883547c7>
2007-01-31 15:25:46 +0000

    Update version number on all relevent files

--- trunk/rsyncrypto.h
+++ trunk/rsyncrypto.h
@@ -7,7 +7,7 @@
 #define DIRSEP_S "/"
 #else
 #define PACKAGE_NAME "rsyncrypto"
-#define PACKAGE_STRING "rsyncrypto 1.02testing2"
+#define PACKAGE_STRING "rsyncrypto 1.02"
 #define DIRSEP_C '\\'
 #define DIRSEP_S "\\"
 #pragma warning ( disable : 4786 )
