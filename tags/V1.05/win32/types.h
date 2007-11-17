ed3a243cf669a513b8cc0f40fcc87da66b57db26
thesun <thesun@eaec4259-e421-0410-a8d7-a676883547c7>
2007-11-17 19:34:52 +0000

    Tag release 1.05

---
+++ tags/V1.05/win32/types.h
@@ -0,0 +1,22 @@
+#if !defined(_WIN32)
+#error Win32 only header included from non-Win32 build environment
+#endif
+
+#ifndef WIN32_TYPES_H
+#define WIN32_TYPES_H
+
+int Error2errno( DWORD Error );
+
+typedef unsigned char uint8_t;
+typedef UINT32 uint32_t;
+typedef unsigned short uint16_t;
+
+#define S_ISREG(m)      (((m) & S_IFMT) == S_IFREG)
+
+static inline void bzero( void *dest, size_t count )
+{
+    memset( dest, 0, count);
+}
+
+
+#endif // WIN32_TYPES_H
