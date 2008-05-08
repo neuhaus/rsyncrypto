ec7c681802d8178ab528ed12cbe5cdf7741bd0fd
thesun <thesun@eaec4259-e421-0410-a8d7-a676883547c7>
2008-05-08 06:08:32 +0000

    Tag changes to the installer generator to work around a visual studio bug

---
+++ tags/V1.07-2/aes_crypt.h
@@ -0,0 +1,47 @@
+#ifndef AES_CRYPT_H
+#define AES_CRYPT_H
+
+#include "crypt_key.h"
+#include "autoarray.h"
+#include <openssl/aes.h>
+
+class aes_key : public key {
+    struct aes_export {
+        unsigned char iv[AES_BLOCK_SIZE];
+        unsigned char key[1];
+    } aes_header;
+    auto_array<unsigned char> secret_key;
+    aes_key( const aes_key &that );
+
+    unsigned char cbc_base[AES_BLOCK_SIZE];
+    AES_KEY encrypt_key, decrypt_key;
+
+    void update_keys()
+    {
+        AES_set_decrypt_key( secret_key.get(), header.key_size*8, &decrypt_key );
+        AES_set_encrypt_key( secret_key.get(), header.key_size*8, &encrypt_key );
+    }
+protected:
+    virtual key *gen_pad_key() const;
+public:
+    aes_key( uint16_t key_size,  uint32_t sum_span, uint32_t sum_mod, uint32_t sum_min_dist,
+            const unsigned char *buffer );
+    aes_key( size_t keybits, uint32_t sum_span, uint32_t sum_mod, uint32_t sum_min_dist );
+    virtual size_t block_size() const
+    {
+        return AES_BLOCK_SIZE;
+    }
+    virtual size_t exported_length() const
+    {
+        return key::exported_length()+header.key_size+block_size();
+    }
+    virtual size_t export_key( void *buffer ) const;
+
+    // Encryption/decryption functions
+    virtual void init_encrypt(); // Reset the IV values
+    // Encrypt/decrypt in place. "size" is not guarenteed to work if bigger than block_size
+    virtual void encrypt_block( unsigned char *data, size_t size );
+    virtual void decrypt_block( unsigned char *data, size_t size );
+};
+
+#endif // AES_CRYPT_H
---
+++ tags/V1.07-2/autommap.h
@@ -0,0 +1,95 @@
+/*
+ * This file is part of rsyncrypto - rsync friendly encryption
+ * Copyright (C) 2005 Shachar Shemesh for Lingnu Open Source Consulting (http://www.lignu.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef _AUTOMMAP_H
+#define _AUTOMMAP_H
+
+#include <errno.h>
+
+// automap will auto-release mmaped areas
+class autommap {
+    void *ptr;
+    size_t size;
+
+    // Disable default copy constructor
+    autommap( const autommap & );
+public:
+    autommap() : ptr(reinterpret_cast<void *>(-1)), size(0)
+    {
+    }
+    autommap(void *start, size_t length, int prot, int flags, int fd, off_t offset ) : 
+                ptr(mmap(start, length, prot, flags, fd, offset)), size(length)
+    {
+        if( ptr==reinterpret_cast<void *>(-1) ) {
+            size=0;
+#if defined(EXCEPT_CLASS)
+            throw EXCEPT_CLASS("mmap failed", errno);
+#endif
+        }
+    }
+    // Map an entire file into memory
+    autommap(file_t fd, int prot) : ptr(reinterpret_cast<void *>(-1)), size(0)
+    {
+        struct stat filestat;
+        if( fstat(fd, &filestat)==0 ) {
+            autommap that(NULL, filestat.st_size, prot, MAP_SHARED, fd, 0);
+            *this=that;
+        }
+#if defined(EXCEPT_CLASS)
+        else
+            throw EXCEPT_CLASS("file mmap failed", errno);
+#endif
+    }
+    ~autommap()
+    {
+        clear();
+    }
+    void *get() const
+    {
+        return ptr;
+    }
+    unsigned char *get_uc() const
+    {
+        return static_cast<unsigned char *>(ptr);
+    }
+    autommap &operator=( autommap &that )
+    {
+        clear();
+        ptr=that.ptr;
+        size=that.size;
+        that.ptr=reinterpret_cast<void *>(-1);
+        that.size=0;
+
+        return *this;
+    }
+    void clear()
+    {
+        if( ptr!=reinterpret_cast<void *>(-1) ) {
+            munmap( ptr, size );
+        }
+        ptr=reinterpret_cast<void *>(-1);
+        size=0;
+    }
+
+    size_t getsize() const {
+        return size;
+    }
+};
+
+#endif // _AUTOMMAP_H
